35bdc434401d7cc932728ec376fcec1300e6350f
paulaandrezza <paulaandrezza25@gmail.com>
2023-04-05 15:55:24 -0300

    Javascript Arrays

---
+++ teste.js
@@ -0,0 +1,16 @@
+// for (let i = 0; i < 5; i++) {
+//   console.log(i)
+// }
+
+// console.log('\n')
+
+// for (let i = 0; i < 5; ++i) {
+//   console.log(i)
+// }
+
+let i = 0
+console.log(i++)
+console.log(i)
+i = 0
+console.log(++i)
+console.log(i)
