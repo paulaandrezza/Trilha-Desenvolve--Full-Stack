be7204a5c6e952295c80d1f8a770c026a256f777
paulaandrezza <paulaandrezza25@gmail.com>
2023-05-22 18:42:35 -0300

    remove console.log of dbConnect.js

--- 18.NodeJs_API_Rest_com_Express_e_MongoDB/src/config/dbConnect.js
+++ 18.NodeJs_API_Rest_com_Express_e_MongoDB/src/config/dbConnect.js
@@ -3,8 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log(process.env.DB_PASSWORD)
-
 mongoose.connect(`mongodb+srv://paulaandrezza:${process.env.DB_PASSWORD}@alura.8kt5hog.mongodb.net/alura-node`);
 
 let db = mongoose.connection;
